f8821f55bca6fa60d591bf7cf46db0031bf942f4
andrearuocco <ruoca96@gmail.com>
2024-09-09 21:19:27 +0200

    startproject

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import morgan from 'morgan'
+import helmet from 'helmet'
+/* import passport from 'passport'
+import GoogleStrategy from './config/passport.config.js' 
+import authorization from './middleware/authorization.js'; */
+import cors from 'cors'
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+const port = process.env.PORT || 5000
+const host = process.env.HOST || 'http://localhost:5000/'
+const server = express()
+
+server.use(express.json())
+server.use(cors()) 
+server.use(morgan("dev")) 
+server.use(helmet())
+
+await mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connessione al database...'))
+    .catch((err) => console.log(err)) 
+
+server.listen(port, () => {
+    console.log(`Server is listening at port ${port} at ${host}`)
+})
